0b3583a3a304bc2ead65fe5ff7a431fd40222d0b
devfernandess <109873686+devfernandess@users.noreply.github.com>
2025-05-14 09:43:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function sube(){
+    var barra = document.querySelector(".sub").style.display = "flex"; 
+
+}
+
+function suba(){
+    var barra = document.querySelector(".sub").style.display = "none"; 
+}
+
+function nave(){
+    nav = document.querySelector(".nav").style.backgroundColor = "white";
+}
+
+function nava(){
+    nav = document.querySelector(".nav").style.backgroundColor = "transparent";
+}
